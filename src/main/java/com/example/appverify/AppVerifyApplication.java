4c82eaad64f5efcd5d71b2de0018ec0517d024fc
liwencong <wencongli72@gmail,.com>
2025-11-11 21:54:40 +0800

    init

---
+++ src/main/java/com/example/appverify/AppVerifyApplication.java
@@ -0,0 +1,11 @@
+package com.example.appverify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVerifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppVerifyApplication.class, args);
+    }
+}
